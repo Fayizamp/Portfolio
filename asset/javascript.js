dc00c749f4a58aa8708bba73e6383037119511ab
fayiza <fayizamp11@gmail.com>
2024-08-06 13:15:51 +0530

    second commit

--- asset/javascript.js
+++ asset/javascript.js
@@ -1,26 +1,5 @@
 
 
-// function sendMail(){
-//     var params = {
-//         name:document.getElementById('name').value ,
-//         email:document.getElementById('emailid').value ,
-//         message:document.getElementById('msg').value
-//     };
-    
-// const serviceID = "service_khd2maq";
-// const templateID="template_3hps30t";
-
-// emailjs.send(serviceID,templateID,params)
-// .then(
-//     res =>{
-//         document.getElementById("name").value="";
-//         document.getElementById("emailid").value="";
-//         document.getElementById("msg").value="";
-//         console.log(res);
-//         alert("your message send successfully")
-//     })
-// .catch((err) => console.log(err));
-// }
 function sendMail() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('emailid').value;
@@ -71,3 +50,5 @@ document.addEventListener('DOMContentLoaded', function() {
     // Attach the event listener
     contactForm.addEventListener('submit', submitFormHandler);
 });
+
+
